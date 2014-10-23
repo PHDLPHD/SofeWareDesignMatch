d3594bc0eca19d674536ca13c1a671ffa6037e98
Mark <294901977@qq.com>
2014-10-23 08:56:59 +0800

    1.更改相应实体类的位置

---
+++ WindowsClient/SDM.WinClient/SDM.DTO/SelectResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDM.DTO
+{
+    public class SelectResult<T>:EventArgs where T:class,new()
+    {
+        public T entity { get; set; }
+        public List<T> entities { get; set; }
+        public SelectResult()
+        {
+            entities = new List<T>();
+            entity = new T();
+        }
+    }
+}
--- WindowsClient/SDM.WinClient/SDM.WinClient/Common/BaseForm.cs
+++ WindowsClient/SDM.WinClient/SDM.WinClient/Common/BaseForm.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using SDM.DTO;
 
 namespace SDM.WinClient.Common
 {
